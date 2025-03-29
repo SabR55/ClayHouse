c5c613aeb56231bc86ae4fc8ce2c17a4c77669d9
SabR55 <sabila.rasyida@gmail.com>
2025-03-29 18:48:45 +0800

    Profile page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Layout from './Layout'
 import Home from './pages/home'
 import Register from './pages/Register'
+import Profile from './pages/UserProfile'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000';
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>} />
         <Route path="/register" element={<Register />}/>
+        <Route path="/profile" element={<Profile />}/>
       </Route>
     </Routes>
     
---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,11 @@
+
+
+function UserProfile() {
+    return(
+        <div>
+            <a className="py-32 w-full block text-center">Profile Page</a>
+        </div>
+    );
+}
+
+export default UserProfile;
